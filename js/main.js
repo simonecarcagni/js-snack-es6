19041a213eb16dca94ed76642d3e3555fce052e7
Simone Carcagnì <simonecarcagni@libero.it>
2023-03-10 18:07:11 +0100

    Snack numero 2 completato

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Snack numero 1
 const containerDom = document.getElementById('container');
 
 const bikeArray = [
@@ -25,10 +26,59 @@ for (let i = 0; i < bikeArray.length; i++){
 
     weightArray.push(peso);
 
-    console.log(weightArray);
 }
 
 
 let lessWeigth = Math.min(...weightArray);
 
 containerDom.innerHTML = `Il peso minore tra le bici è di: ${lessWeigth} kg`;
+
+
+//Snack numero 2
+
+const squadreCalcio = [
+    {
+        nome : 'Milan',
+        'punti fatti' : 0,
+        'falli subiti' : 0,
+    },
+    {
+        nome: 'Juve',
+        'punti fatti' : 0,
+        'falli subiti' : 0,
+    },
+    {
+        nome: 'Inter',
+        'punti fatti' : 0,
+        'falli subiti' : 0,
+    },
+
+]
+
+function generatoreNumeriCasuali(max , min){
+    let randomNumber = Math.floor(Math.random(max - min + 1) * max);
+
+    return randomNumber;
+}
+
+let newTeamArray = [];
+
+for (let i = 0; i < squadreCalcio.length; i++){
+    let squadreCalcioIndex = squadreCalcio[i];
+
+    let numberPoints = generatoreNumeriCasuali(10, 1);
+
+    let numberFoul = generatoreNumeriCasuali(5, 1);
+
+    squadreCalcioIndex['punti fatti'] = numberPoints;
+
+    squadreCalcioIndex['falli subiti'] = numberFoul;
+
+    const { nome, 'falli subiti':foul } = squadreCalcioIndex;
+
+    newTeamArray.push(nome , foul);
+
+}
+
+
+console.log(newTeamArray);
